2fb89a9545afff66e06f39037213cd53aa4bc85a
Cesar Acual <chechaacual@gmail.com>
2020-08-04 12:10:21 -0600

    Consumiendo informacion del servicio de Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,10 +16,7 @@ export class SpotifyService {
       'Authorization': 'Bearer BQBNroI2EPXBW-DCjQGWzhACCTXHxHc1i_9sif80clx2EPVq35vrlDDZG8JM2KtP_gsjrjW2m-SBS224oNE'
     })
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-      .subscribe( data => {
-        console.log(data);
-      });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
 
   }
 }

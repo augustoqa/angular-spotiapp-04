dc18e386497f43e78f2721b7ae3e929833706ced
Cesar Acual <chechaacual@gmail.com>
2020-08-12 11:42:01 -0600

    Componente de Busqueda de artista

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,10 +13,18 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBNroI2EPXBW-DCjQGWzhACCTXHxHc1i_9sif80clx2EPVq35vrlDDZG8JM2KtP_gsjrjW2m-SBS224oNE'
+      'Authorization': 'Bearer BQAd1folrFcb1v0dl0Td381VAFT8LSzw-VFu6ZR75bV-Ei_r2312NMLvERHF9jeqaIVpWLejU4ukXM6qYiM'
     })
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
 
+  }
+
+  getArtista( termino: string ) {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQAd1folrFcb1v0dl0Td381VAFT8LSzw-VFu6ZR75bV-Ei_r2312NMLvERHF9jeqaIVpWLejU4ukXM6qYiM'
+    })
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
   }
 }
